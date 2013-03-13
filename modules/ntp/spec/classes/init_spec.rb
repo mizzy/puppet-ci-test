bc54728fd57225edf7aaf526b7f47d9d2628ecd1
Gosuke Miyashita <gosukenator@gmail.com>
2013-03-13 23:20:28 +0900

    fail test

--- modules/ntp/spec/classes/init_spec.rb
+++ modules/ntp/spec/classes/init_spec.rb
@@ -3,4 +3,5 @@ require 'spec_helper'
 describe 'ntp' do
   it { should include_class('ntp::install') }
   it { should include_class('ntp::service') }
+  it { should include_class('ntp::config') }
 end
